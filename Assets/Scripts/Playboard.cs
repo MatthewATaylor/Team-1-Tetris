906b0c13a42acae013b4297133e35aa17ea09328
F1gN3wt0n <52759591+F1gN3wt0n@users.noreply.github.com>
2021-01-31 13:44:55 -0500

    Add files via upload
    added TilePlacedAtTransform

--- Assets/Scripts/Playboard.cs
+++ Assets/Scripts/Playboard.cs
@@ -2,26 +2,32 @@
 
 public class Playboard : MonoBehaviour
 {
-    public static int w = 10;
-    public static int h = 20;
-    public static Transform[,] grid = new Transform[w, h];
+ 	public static int w = 10;
+	public static int h = 20;
+	public static Transform[,] grid = new Transform[w, h];
 
-    void Start()
-    {
-        //run block spawn code
-    }
+	private Score score;
 
-    void Update()
-    {
-        //gameplay
-        //when block dropped, run UpdateScoreDrop()
-        //if line cleared, run UpdateScoreRowClear(int num_lines)
-        //run UpdateLevel(int num_lines)
-    }
+	void Start()
+ 	{
+        	score = GameObject.Find(GlobalNames.score).GetComponent<Score>();
+    	}
 
-    public bool TilePlacedAtTransform(Transform transform)
-    {
-        // Check if a transform has been placed in grid at same location as transform argument
-        return false;
-    }
+	void Update()
+    	{
+		//when block dropped, update grid
+        	//when block dropped, run UpdateScoreDrop()
+        	//if line cleared, run UpdateScoreRowClear(int num_lines)
+ 		//run UpdateLevel(int num_lines)
+	}
+
+	public bool TilePlacedAtTransform(Transform transform)
+  	{
+		foreach (Transform child in transform){
+			if (Playboard.grid[(int)child.position.x, (int)child.position.y] != null){
+				return false;
+			}
+		}
+        	return true;
+	}
 }
